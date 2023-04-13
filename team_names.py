b8352931400185b0bc1be0d79d7a4881307ce934
adityaanilshukla <adityaanilsindhunath@gmail.com>
2023-04-13 17:40:55 +0800

    First commit before pushing to github

---
+++ team_names.py
@@ -0,0 +1,25 @@
+#team names historical and present
+#these will be used to re name teams from web scraped tables
+
+team_names_2015 = [ 'Mercedes','Ferrari','Williams Mercedes','Red Bull Racing Renault',
+                    'Force India Mercedes','Lotus Mercedes','STR Renault',
+                    'Sauber Ferrari','McLaren Honda','Marussia Ferrari']
+
+team_names_2016 = [ 'Mercedes','Ferrari','Williams Mercedes','Red Bull Racing TAG Heuer'
+                    ,'Force India Mercedes','Renault','Toro Rosso Ferrari'
+                    ,'Sauber Ferrari','McLaren Honda','Haas Ferrari']
+
+team_names_2017 = [ 'Mercedes','Ferrari','Williams Mercedes','Red Bull Racing TAG Heuer'
+                    ,'Force India Mercedes','Renault','Toro Rosso'
+                    ,'Sauber Ferrari','McLaren Honda','Haas Ferrari']
+
+team_names_2018 = [ 'Mercedes','Ferrari','Williams Mercedes','Red Bull Racing TAG Heuer'
+                    ,'Force India Mercedes','Renault','Scuderia Toro Rosso Honda'
+                    ,'Sauber Ferrari','McLaren Renault','Haas Ferrari']
+
+team_names_2019 = [ 'Mercedes','Ferrari','Williams Mercedes','Red Bull Racing Honda'
+                    ,'Racing Point BWT Mercedes','Renault','Scuderia Toro Rosso Honda'
+                    ,'Alfa Romeo Racing Ferrari','McLaren Renault','Haas Ferrari']
+
+team_names_current = ['Mercedes','Ferrari','Williams','RedBull Racing','Aston Martin','Alpine',
+                      'AlphaTauri','Alfa Romeo','McLaren','Haas']
---
+++ urls.py
@@ -0,0 +1,43 @@
+# urls for qualifying sessions in singapore, monaco, japan(suzuka) and abu dhabi between 2015-2019
+sg_2015_q = "https://www.formula1.com/en/results.html/2015/races/930/singapore/qualifying.html"
+sg_2016_q = "https://www.formula1.com/en/results.html/2016/races/951/singapore/qualifying.html"
+sg_2017_q = "https://www.formula1.com/en/results.html/2017/races/972/singapore/qualifying.html"
+sg_2018_q = "https://www.formula1.com/en/results.html/2018/races/993/singapore/qualifying.html"
+sg_2019_q = "https://www.formula1.com/en/results.html/2019/races/1014/singapore/qualifying.html"
+
+mon_2015_q = "https://www.formula1.com/en/results.html/2015/races/922/monaco/qualifying.html"
+mon_2016_q = "https://www.formula1.com/en/results.html/2016/races/943/monaco/qualifying.html"
+mon_2017_q = "https://www.formula1.com/en/results.html/2017/races/964/monaco/qualifying.html"
+mon_2018_q = "https://www.formula1.com/en/results.html/2018/races/984/monaco/qualifying.html"
+mon_2019_q = "https://www.formula1.com/en/results.html/2019/races/1005/monaco/qualifying.html"
+
+suz_2015_q = "https://www.formula1.com/en/results.html/2015/races/931/japan/qualifying.html"
+suz_2016_q = "https://www.formula1.com/en/results.html/2016/races/953/japan/qualifying.html"
+suz_2017_q = "https://www.formula1.com/en/results.html/2017/races/974/japan/qualifying.html"
+suz_2018_q = "https://www.formula1.com/en/results.html/2018/races/995/japan/qualifying.html"
+suz_2019_q = "https://www.formula1.com/en/results.html/2019/races/1016/japan/qualifying.html"
+
+abu_2015_q = "https://www.formula1.com/en/results.html/2015/races/936/abu-dhabi/qualifying.html"
+abu_2016_q = "https://www.formula1.com/en/results.html/2016/races/957/abu-dhabi/qualifying.html"
+abu_2017_q = "https://www.formula1.com/en/results.html/2017/races/978/abu-dhabi/qualifying.html"
+abu_2018_q = "https://www.formula1.com/en/results.html/2018/races/999/abu-dhabi/qualifying.html"
+abu_2019_q = "https://www.formula1.com/en/results.html/2019/races/1020/abu-dhabi/qualifying.html"
+
+#urls for the the points earned by each team between 2015-2019
+constructorUrl_2015 = "https://www.formula1.com/en/results.html/2015/team.html"
+constructorUrl_2016 = "https://www.formula1.com/en/results.html/2016/team.html"
+constructorUrl_2017 = "https://www.formula1.com/en/results.html/2017/team.html"
+constructorUrl_2018 = "https://www.formula1.com/en/results.html/2018/team.html"
+constructorUrl_2019 = "https://www.formula1.com/en/results.html/2019/team.html"
+
+
+#main program will extract the urls by looping throught these lists
+singporeQuali_15_19 = [sg_2015_q,sg_2016_q,sg_2017_q,sg_2018_q,sg_2019_q]
+
+monacoQuali_15_19 = [mon_2015_q, mon_2016_q, mon_2017_q, mon_2018_q, mon_2019_q]
+
+japanQuali_15_19 = [suz_2015_q, suz_2016_q, suz_2017_q, suz_2018_q, suz_2019_q]
+
+AbuDhabiQuali_15_19 = [abu_2015_q, abu_2016_q, abu_2017_q, abu_2018_q, abu_2019_q]
+
+constructors_points_15_19 = [constructorUrl_2015, constructorUrl_2016, constructorUrl_2017, constructorUrl_2018, constructorUrl_2019]
